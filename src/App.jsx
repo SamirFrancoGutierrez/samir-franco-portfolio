e02fc0a64b3e714204bdf424d2d8a5cd5219a345
Anthony Samir Franco Gutierrez <117482264+SamirFrancoGutierrez@users.noreply.github.com>
2023-11-06 20:49:31 -0600

    resolved bugs

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
         <Route path='about' element={<About />} />
         <Route path='portfolio' element={<Portfolio />} />
         <Route path='contact' element={<Contact />} />
+        <Route path='samir-franco-portfolio' element={<Home />} />
       </Routes>
   </BrowserRouter>
   );
