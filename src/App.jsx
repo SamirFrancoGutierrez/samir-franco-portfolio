c8b1eb81af416fb6bae1ef794a1ce6f790179131
Anthony Samir Franco Gutierrez <117482264+SamirFrancoGutierrez@users.noreply.github.com>
2023-11-06 21:10:58 -0600

    add domain

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path='about' element={<About />} />
         <Route path='portfolio' element={<Portfolio />} />
         <Route path='contact' element={<Contact />} />
-        <Route path='samir-franco-portfolio' element={<Home />} />
+        <Route path='samirfranco.tech' element={<Home />} />
       </Routes>
   </BrowserRouter>
   );
