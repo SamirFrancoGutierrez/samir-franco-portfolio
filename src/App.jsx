7037747a1c1ef84e8e8c2fd679d2d0582be7afa4
Anthony Samir Franco Gutierrez <117482264+SamirFrancoGutierrez@users.noreply.github.com>
2023-11-06 20:21:05 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
         <Route path='about' element={<About />} />
         <Route path='portfolio' element={<Portfolio />} />
         <Route path='contact' element={<Contact />} />
+        <Route path='http://samirfranco.tech/' element={<Home />} />
       </Routes>
   </BrowserRouter>
   );
