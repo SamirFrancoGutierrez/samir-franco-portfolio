819be88c0f2d1adae66e986221e76576f05dc5dd
Anthony Samir Franco Gutierrez <117482264+SamirFrancoGutierrez@users.noreply.github.com>
2023-11-06 20:49:46 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"http://samirfranco.tech/",
+  base:"https://portafolio.samirfranco.tech",
   esbuild: {
     
     target: 'es6'
